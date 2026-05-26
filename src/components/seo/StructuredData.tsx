95b7636f94a7fb7f8ddd90384f9a0de49d3eeec0
Mohammed Arif <mohammedarif@Mac.hgu_lan>
2026-05-26 14:55:12 +0530

    fix(schema): update HospitalSchema maps link to client verified maps app shortlink

--- src/components/seo/StructuredData.tsx
+++ src/components/seo/StructuredData.tsx
@@ -22,7 +22,7 @@ export const HospitalSchema = () => {
       "longitude": 78.1460
     },
     "openingHours": "00:00-23:59",
-    "hasMap": "https://maps.google.com/?cid=10646698621415257840",
+    "hasMap": "https://maps.app.goo.gl/c4oHsAMwjLq9UqYi8",
     "medicalSpecialty": [
       "Orthopedic Surgery",
       "Sports Medicine",
